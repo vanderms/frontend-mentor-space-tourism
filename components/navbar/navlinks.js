e950d3286230298dbb6bb2950a585945dd8716af
vanderms <vanderms.84@gmail.com>
2021-12-26 18:56:58 -0300

    replace equal sign by indexOf in navlinks

--- components/navbar/navlinks.js
+++ components/navbar/navlinks.js
@@ -11,13 +11,13 @@ export default function NavLinks({className}){
       <li className={router.pathname === '/' ? 'active' : null}>
         <Link href='/'><a><span className="number">00 </span>HOME</a></Link>
       </li>
-      <li className={router.pathname === '/destination' ? 'active' : null}>
+      <li className={router.pathname.indexOf('/destination') === 0 ? 'active' : null}>
         <Link href='/destination/moon'><a><span className="number">01 </span>DESTINATION</a></Link>
       </li>
-      <li className={router.pathname === '/crew' ? 'active' : null}>
+      <li className={router.pathname.indexOf('/crew') === 0 ? 'active' : null}>
         <Link href='/crew'><a><span className="number">02 </span>CREW</a></Link>
       </li>
-      <li className={router.pathname === '/technology' ? 'active' : null}>
+      <li className={router.pathname.indexOf('/technology') === 0 ? 'active' : null}>
         <Link href='/technology'><a><span className="number">03 </span>TECHNOLOGY</a></Link>
       </li>
     </ul>
